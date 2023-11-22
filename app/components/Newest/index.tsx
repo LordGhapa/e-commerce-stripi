3240104df27cff9625ad2e6e6dbdb01bf3579f28
Felipe Lacerda <99082399+LordGhapa@users.noreply.github.com>
2023-11-21 23:24:45 -0300

    💄 UI: create section newest product in index

---
+++ app/components/Newest/index.tsx
@@ -0,0 +1,74 @@
+import { client } from "@/app/lib/sanity";
+import { simplifiedProduct } from "@/app/types/interface";
+import { ArrowRight } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+async function getData() {
+  const query = `*[_type=="production"][0...4]|order(_createdAt asc){
+  _id,
+    price,
+    name,
+    "slug":slug.current,
+    "categoryName":category->name,
+    "imagesUrl":images[0].asset->url
+}`;
+  const data = await client.fetch(query);
+  return data;
+}
+
+export default async function Newest() {
+  const data: simplifiedProduct[] = await getData();
+
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <div className="flex items-center justify-between">
+          <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+            Our Newest products
+          </h2>
+
+          <Link className="flex items-center gap-x-1 text-primary" href="/all">
+            See All
+            <span>
+              <ArrowRight />
+            </span>
+          </Link>
+        </div>
+
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {data.map((product) => (
+            <div key={product._id} className="group relative">
+              <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
+                <Image
+                  src={product.imagesUrl}
+                  alt="Product image"
+                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  width={300}
+                  height={300}
+                />
+              </div>
+
+              <div className="">
+                <Link
+                  className="mt-4 flex justify-between"
+                  href={`/product/${product.slug}`}
+                >
+                  <div>
+                    <h3 className="text-sm text-gray-700">{product.name}</h3>
+                    <p className="mt-1 text-sm text-gray-500">
+                      {product.categoryName}
+                    </p>
+                  </div>
+                  <p className="text-sm font-medium text-gray-900">
+                    {`R$${product.price}`}
+                  </p>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/types/interface.ts
@@ -0,0 +1,8 @@
+export interface simplifiedProduct {
+  _id: string;
+  imagesUrl: string;
+  price: number;
+  name: string;
+  slug: string;
+  categoryName: string;
+}
